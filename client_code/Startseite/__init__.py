4ebcea079a313a22a152b6568c8a8d9abe3bf257
ayo.mohamad@hotmail.com <ayo.mohamad@hotmail.com>
2024-10-09 09:43:47 +0000

    Created Startseite and Database_Server; Edited settings; Deleted Form1

---
+++ client_code/Startseite/__init__.py
@@ -0,0 +1,28 @@
+from ._anvil_designer import StartseiteTemplate
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+
+class Startseite(StartseiteTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+    item_list = []
+    for row in [
+      {"name":"Feldkirch"}, 
+      {"name":"Bludenz"},
+      {"name":"Bregenz"},
+      {"name":"Dornbirn"}]:
+        item_list.append((row["name"], row))
+    self.drop_down_1.items = item_list
+
+    print(anvil.server.call("say_hello", "Peter"))
+
+  def drop_down_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    pass
