93c07c4efe1cbd8efcc6091120a14c6553fec574
mathias.florea@student.htl-rankweil.at <mathias.florea@student.htl-rankweil.at>
2024-10-26 10:26:05 +0000

    Edited Book, Booking and Statistics

--- client_code/Book/__init__.py
+++ client_code/Book/__init__.py
@@ -5,15 +5,26 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
+"""from ..Home import Home
+from ..Statistics import Statistics"""
 
 class Book(BookTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-  
-  def button_login_register_click(self, **event_args):
-    open_form('LoginRegister')
+"""
+  def link_home_click(self, **event_args):
+    self.content_panel.clear()
+    self.content_panel.add_component(Home())
     pass
+
+  def link_book_click(self, **event_args):
+    pass
+
+  def link_statistics_click(self, **event_args):
+    self.content_panel.clear()
+    self.content_panel.add_component(Statistics())
+    pass"""
 
   
   
--- client_code/Statistics/__init__.py
+++ client_code/Statistics/__init__.py
@@ -6,6 +6,8 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+from ..Book import Book
+from ..Home import Home
 
 class Statistics(StatisticsTemplate):
   def __init__(self, **properties):
@@ -13,3 +15,16 @@ class Statistics(StatisticsTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_statistics_click(self, **event_args):
+    pass
+
+  def link_home_click(self, **event_args):
+    self.content_panel.clear()
+    self.content_panel.add_component(Home())
+    pass
+
+  def link_book_click(self, **event_args):
+    self.content_panel.clear()
+    self.content_panel.add_component(Book())
+    pass
